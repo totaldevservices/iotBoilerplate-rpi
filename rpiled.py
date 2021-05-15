55ac36660c0f09345e413a1b9c24c14e1317e86d
totaldevservices <81661644+totaldevservices@users.noreply.github.com>
2021-05-14 23:39:45 -0400

    Initial Commit

---
+++ rpiled.py
@@ -0,0 +1,47 @@
+from bluetooth import *
+import RPi.GPIO as GPIO
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "SampleServer",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
+                    )
+                   
+print("Waiting for connection on RFCOMM channel %d" % port)
+
+client_sock, client_info = server_sock.accept()
+print("Accepted connection from ", client_info)
+
+try:
+    while True:
+        data = client_sock.recv(1024)
+        if len(data) == 0: break
+        
+        if data == "on":
+            print("received [%s]" % data)
+            GPIO.output(8, GPIO.HIGH) # Turn LED on at GPIO 8
+        elif data == "off":
+            print("received [%s]" % data)
+            GPIO.output(8, GPIO.LOW) # Turn LED off at GPIO 8
+
+except IOError:
+    pass
+
+print("disconnected")
+
+client_sock.close()
+server_sock.close()
+print("all done")
