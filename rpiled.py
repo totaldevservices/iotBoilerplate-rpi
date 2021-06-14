88d2e1cfb6bb39455f1ac6c8d8692fb1ea55b6db
totaldevservices <tdstotaldevservices@gmail.com>
2021-06-13 23:03:47 -0400

    Implemented multithreading; ability to send and receive data simultaneously

--- rpiled.py
+++ rpiled.py
@@ -1,47 +1,68 @@
+# importing required libraries
 from bluetooth import *
 import RPi.GPIO as GPIO
+import threading
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 
-server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
+# Set pin 8 to be an output pin and set initial value to low (off)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
+
+server_sock = BluetoothSocket(RFCOMM)  # declaring server socket
+server_sock.bind(("", PORT_ANY))  # declaring port binding
 server_sock.listen(1)
 
+# Retrieving socket name
 port = server_sock.getsockname()[1]
 
+# BT UUID
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service( server_sock, "SampleServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
-                   
+# Declaring Bluetooth Advertising Service in order for other Bluetooth device to find the SBC's Bluetooth
+advertise_service(server_sock, "RPI_BT_Server",
+                  service_id=uuid,
+                  service_classes=[uuid, SERIAL_PORT_CLASS],
+                  profiles=[SERIAL_PORT_PROFILE],
+                  )
+
 print("Waiting for connection on RFCOMM channel %d" % port)
 
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from ", client_info)
 
-try:
+
+# Receiving data function
+def recv_data():
     while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        
-        if data == "on":
-            print("received [%s]" % data)
-            GPIO.output(8, GPIO.HIGH) # Turn LED on at GPIO 8
-        elif data == "off":
-            print("received [%s]" % data)
-            GPIO.output(8, GPIO.LOW) # Turn LED off at GPIO 8
+        recv_data = client_sock.recv(1024)
 
-except IOError:
-    pass
+        if not recv_data:
+            sys.exit(0)
 
-print("disconnected")
+        if recv_data == "on":
+            print("received [%s]" % recv_data)
+            GPIO.output(8, GPIO.HIGH)  # Turn LED on at GPIO 8
 
-client_sock.close()
-server_sock.close()
-print("all done")
+        elif recv_data == "off":
+            print("received [%s]" % recv_data)
+            GPIO.output(8, GPIO.LOW)  # Turn LED off at GPIO 8
+
+        print("received [%s]" % recv_data)
+
+# Sending data thread
+
+
+def send_data():
+    while True:
+        send_data = raw_input("Enter message: ")
+        client_sock.send(send_data)
+
+# Since we're both receiving and sending data simultaneously, we'll need a thread for each functionality:
+# a thread for receiving data and a thread for sending data
+
+
+# Start multithreading
+t = threading.Thread(target=recv_data)
+t.start()
+send_data()
